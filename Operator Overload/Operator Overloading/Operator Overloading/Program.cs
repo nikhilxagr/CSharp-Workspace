289f47b8580e6ee2637d9b2b388505e6e2161636
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-03-11 23:30:04 +0530

    Add Operator Overloading C# project
    Create a new Operator Overloading project and solution. Adds solution file, .csproj (targeting .NET Framework 4.7.2) with references and build configs, App.config, Properties/AssemblyInfo.cs, and Program.cs containing a sample Space class and a unary - operator overload demo (SpaceTest Main).

---
+++ Operator Overload/Operator Overloading/Operator Overloading/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Operator_Overloading
+{
+    internal class Program
+    {
+        int x, y, z;
+        public Space(int a, int b, int c)
+        {
+            x = a;
+            y = b;
+            z = c;
+        }
+        public void display()
+        {
+            Console.Write("" + x );
+            Console.Write("" + y);
+            Console.Write("" + z);
+            Console.WriteLine();
+        }
+        public static Space operator - (Space s)
+        {
+            s.x = -s.x;
+            s.y = -s.y;
+            s.z = -s.z;
+        }
+        class SpaceTest
+        {
+            public static void Main(string[] args)
+            {
+                Space s = new Space(10, -20, 30);
+                Console.WriteLine("s:");
+                s.display();
+
+                -s:  // Activates the operator -() method
+                
+                Console.WriteLine("s:");
+                s.display();
+
+            }
+        }
+}
