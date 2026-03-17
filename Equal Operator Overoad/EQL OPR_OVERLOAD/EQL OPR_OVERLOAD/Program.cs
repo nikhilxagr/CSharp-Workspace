021586436ec0efe9d4e33838214ed3414cdc3be7
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-03-17 17:33:11 +0530

    Add Fraction operator-overload
    Add a new C# console project demonstrating operator overloading for a Fraction type. Adds solution, csproj, App.config, AssemblyInfo and Program.cs. Program implements Fraction with overloaded +, == and !=, ToString and a Tester.Main that exercises them. Note: equality currently compares numerator and denominator directly, so equivalent fractions like 1/2 and 2/4 are not treated as equal.

---
+++ Equal Operator Overoad/EQL OPR_OVERLOAD/EQL OPR_OVERLOAD/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQL_OPR_OVERLOAD
+{
+    internal class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        // overload operator + taken two fractions and return a new fraction
+        public static Fraction operator +(Fraction lhs, Fraction rhs)
+        {
+           if (lhs.numerator == rhs.numerator)
+            {
+                return new Fraction(lhs.numerator + lhs.numerator , rhs.denominator);
+            }
+        int firstProduct = lhs.numerator * rhs.denominator;
+        int secondProduct = rhs.numerator * lhs.denominator;
+            return new Fraction(firstProduct + secondProduct, lhs.denominator * rhs.denominator);
+        }
+
+        // delegates to the == operator to check whether two Fractions Are Equal
+
+        public static bool operator ==(Fraction lhs, Fraction rhs)
+        {
+           if (lhs.denominator == rhs.denominator && lhs.numerator == rhs.numerator)
+            {
+                return true;
+            }
+           // code is not working for 1/2 and 2/4 ( unlike fraction ) 
+            return false;
+
+        }
+
+        // Check whether two Fractions Are Not Equal
+
+        public static bool operator !=(Fraction lhs, Fraction rhs)
+        {
+            return !(lhs == rhs);
+        }
+        public override string ToString()
+        {
+            string s = numerator.ToString() + "/" + denominator.ToString();
+            return s;
+        }
+
+    }
+    public class Tester 
+    {
+       public void Run()
+        {
+            Fraction f1 = new Fraction(3, 4);
+            Console.WriteLine("f1:{0}", f1.ToString());
+            
+            Fraction f2 = new Fraction(2, 4);
+            Console.WriteLine("f2:{0}", f2.ToString());
+
+            Fraction f3 = f1 + f2;
+
+            Console.WriteLine("f1 + f2 = {0}", f3.ToString());
+
+            Fraction f4 = new Fraction(5 ,4);
+
+            if (f4 == f3)
+            {
+                Console.WriteLine("f4:{0} == f3:{1}", f4.ToString(), f3.ToString());
+
+            }
+            if (f4 != f2)
+            {
+                Console.WriteLine("f4:{0} != f2:{1}", f4.ToString(), f2.ToString());
+            }
+
+            if (f4.Equals(f3))
+            {
+                Console.WriteLine("f4:{0} Equals f3:{1}", f4.ToString(), f3.ToString());
+            }
+        }
+        static void Main()
+        {
+            Tester t = new Tester();
+            t.Run();
+        }
+    }
+}
